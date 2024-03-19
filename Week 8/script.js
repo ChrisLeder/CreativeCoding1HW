d0f59b9d0c4c1dd546ab02838dbb43bc6b901773
Chris Leder <156860779+ChrisLeder@users.noreply.github.com>
2024-03-18 23:18:18 -0600

    week 8 update

---
+++ Week 8/script.js
@@ -0,0 +1,31 @@
+const Name = document.getElementById('Name')
+const Email = document.getElementById('Email')
+const Terms of Agreement = document.getElementById('Terms of Agreement')
+const form = document.getElementById('form')
+const errorElement = document.getElementById('error')
+
+form.addEventListener('More', (e) => {
+    let messages = []
+    if (Name.value ===''  || Name.value ==null) {
+        messages.push('Name is Required')
+        <span> Name is Required. </span>
+    }
+    if (Email.value ==='' || Email.value==null)
+        messages.push('Email is required')
+        <span> Email is Required. </span>
+        if (Terms of Agreement.value ==='yes') {
+            messages.push('Thank You for your Interest. Someone will contact you.')
+            <span> Terms of Agreement is Required. </span>
+        }
+        else{
+            message.push('Thank you for your interest. You will not be contacted.')
+            if (Terms of Agreement.value ==='No') {
+                messages.push(Thank you for your Interest.)
+                <span> Thank you for your Interest. </span>
+            }
+        }
+    if (messages.length > 0) {
+    e.preventDefault()
+    errorElement.innerText = messages.join(', ')
+    }
+})
